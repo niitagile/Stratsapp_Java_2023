04ec3bd7406b9356beaa358db1945ac28be91f06
niitagile <43024343+niitagile@users.noreply.github.com>
2023-09-14 15:43:41 +0530

    Add files via upload

---
+++ WebDevelopment/Servlet_JSP/FirstServletExample/src/main/java/Login.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname=request.getParameter("txtuser");
+		String pword=request.getParameter("txtpword");
+		
+		PrintWriter out =response.getWriter();
+		out.println("Welcome User"+uname);
+	}
+
+}
