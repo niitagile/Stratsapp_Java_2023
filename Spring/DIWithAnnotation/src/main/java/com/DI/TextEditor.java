35f9297d3dd97a943b5e36935043561af81c691d
niitagile <43024343+niitagile@users.noreply.github.com>
2023-09-26 14:59:03 +0530

    Add files via upload

---
+++ Spring/DIWithAnnotation/src/main/java/com/DI/TextEditor.java
@@ -0,0 +1,34 @@
+package com.DI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TextEditor {
+	//dependency
+	@Autowired
+	private SpellChecker spellchecker;
+	
+	
+	public SpellChecker getSpellchecker() {
+		return spellchecker;
+	}
+	//@Autowired
+	public void setSpellchecker(SpellChecker spellchecker) {
+		this.spellchecker = spellchecker;
+	}
+	//@Autowired
+	public TextEditor(SpellChecker spellchecker) {
+		super();
+		this.spellchecker = spellchecker;
+	}
+	public TextEditor() {
+		System.out.println("TextEditor Constructor");
+	}
+	
+	public void spellcheck() {
+		spellchecker.checkSpelling();
+	}
+	
+
+}
